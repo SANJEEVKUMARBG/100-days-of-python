96b797d1913ab59124e5cb892bbadd20986061ba
B G Sanjeev Kumar <39961287+SANJEEVKUMARBG@users.noreply.github.com>
2023-02-06 12:00:34 +0530

    Add files via upload
    This code generated a band name given your city name and your pet name.

---
+++ Day 1_Band_name_generator.py
@@ -0,0 +1,8 @@
+#Greeting
+print("Welcome to band name generator.")
+#Asking for the city name
+city = input("What's your city name?\n")
+#Asking for the pet name
+pet = input("What's your pet name?\n")
+#Creating a band name using both the variables
+print("Your band name can be "+city+"'s'"+" "+pet+" . Hope you liked it")
